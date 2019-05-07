725c84f44c6cc97a4a0793e44b867447dc425664
dianbanjiu <dianbanjiu@gmail.com>
2019-05-07 23:34:17 +0800

    flipping image

--- leetcode/ArrayRemoveDuplicates.go
+++ leetcode/ArrayRemoveDuplicates.go
@@ -2,13 +2,22 @@ package leetcode
 
 // 给定一个排好序的数组，去掉其中重复出现的元素，使得每个元素在数组中仅出现一次，返回新数组的长度
 func ArrayRemoveDuplicates(nums []int) int {
-	for i := 0; i < len(nums)-1; i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[j] == nums[i] {
-				nums = append(nums[:i+1], nums[j+1:]...)
-				j = i
-			}
 
+	/*
+		for i := 0; i < len(nums)-1; i++ {
+			for j := i + 1; j < len(nums); j++ {
+				if nums[j] == nums[i] {
+					nums = append(nums[:j], nums[j+1:]...)
+					j = i
+				}
+
+			}
+		}
+	*/
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i+1] == nums[i] {
+			nums = append(nums[:i], nums[i+1:]...)
+			i--
 		}
 	}
 
